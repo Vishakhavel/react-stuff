d74f3b1a41e644cd1fdc80a1ce979a8eab988231
vishakhavel <vishakhavel.s@capillarytech.com>
2022-12-07 00:46:00 +0530

    basics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-// import NumberCounter from './components/NumberCounter'
-import Display from './components/Display'
-import Form from './components/Form'
-import { Screen } from './styles'
-function App() {
+import { useState, useEffect } from 'react'
+import { DEV_API_ENDPOINT } from './urls'
+const App = () => {
+  const [data, setData] = useState([])
+  const [showData, setShowData] = useState(true)
+  const [buttonMessage, setButtonMessage] = useState('Hide')
+
+  const handleClick = () => {
+    setShowData(!showData)
+    setButtonMessage(buttonMessage === 'Hide' ? 'Show' : 'Hide')
+  }
+
+  useEffect(() => {
+    fetch(DEV_API_ENDPOINT)
+      .then((response) => {
+        return response.status === 200 ? response.json() : []
+      })
+      .then((data) => {
+        setData(data)
+      })
+  }, [])
+
+  const element =
+    data &&
+    data.map((item) => (
+      <tr>
+        <td>{item.userId}</td>
+        <td>{item.title}</td>
+        <td>{item.body}</td>
+      </tr>
+    ))
   return (
-    <Screen>
-      <Form />
-    </Screen>
+    <>
+      <button onClick={handleClick}>{buttonMessage}</button>
+      <table>{showData && element}</table>
+    </>
   )
 }
 
---
+++ src/urls.js
@@ -0,0 +1,1 @@
+export const DEV_API_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts'
